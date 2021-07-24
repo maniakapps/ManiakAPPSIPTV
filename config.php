e8c1ae9e7f9a356f5cea53226eef1badb60f8f7e
BuildTools <unconfigured@null.spigotmc.org>
2021-07-24 03:23:16 -0500

    Primer versión

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php
+#Config file
+//Site Title
+$title = "ManiakAPPS IPTV streamer";
+
+//Site URL
+$site_url = "maniakapps.com";
+
+//Site Description
+$description = "We made it simplier to stream your favorite prorgams";
+
+//Watermark image
+$watermarkImage = "";
+
+//Watermark link
+$watermarkLink = "";
+
+//Google Analytics tag ID
+$gaCode = "";
+
+//Preroll Ads Video (.mp4)
+$prerollAds = "";
+
+//Preroll Ads Time (seconds)
+$prerollTime = 5;
+
+//Preroll Ads External Link
+$prerollLink = "";
+?>
